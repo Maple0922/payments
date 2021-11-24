2bded0c6e9f46e45f723a12bbe3517e75a5a71ca
Maple0922 <maple0922sept@gmail.com>
2021-11-25 01:42:23 +0900

    Update: firebase settings

---
+++ plugins/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC1RM9fGjh3nAu8f0NR02vQuxuEyhgbSKo',
+  authDomain: 'payments-f75d0.firebaseapp.com',
+  projectId: 'payments-f75d0',
+  storageBucket: 'payments-f75d0.appspot.com',
+  messagingSenderId: '443279109636',
+  appId: '1:443279109636:web:cbbaa5b783a6b1832d8f51',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig) // eslint-disable-line no-unused-vars
